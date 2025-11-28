faca74f7b6cd1ee4c256375ba1324a8c3eabd732
Vineeth-Reddy1811 <vineethreddyanugu@gmail.com>
2025-11-28 13:47:53 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Vineeth  Reddy on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
